c82b2aa540e5f0e5499f1a182165c8ac08b47251
digitalbase <gijs@digitalbase.eu>
2025-07-16 09:52:43 +0200

    Add test for emoticon

---
+++ tests/02_emoji/emoticon_test.ts
@@ -0,0 +1,36 @@
+import { assertObjectMatch } from "jsr:@std/assert";
+
+import { basename } from "https://deno.land/std/path/mod.ts";
+import {parseMarkdown} from "../../src/parser.ts";
+import {getFilenameWithoutExtension} from "../utils.ts";
+
+Deno.test("emoticon test", async () => {
+  const testName = getFilenameWithoutExtension(import.meta.filename ?? '');
+  const dirName = basename(import.meta.dirname ?? '');
+  const markDown = await Deno.readTextFile(`./tests/${dirName}/${testName}.md`);
+  const json = {
+          type: "root",
+          children: [
+              {
+                  type: "paragraph",
+                  children: [
+                      { type: "text", value: "This is " },
+                      { type: "text", value: "😶" }
+                  ],
+                  position: {
+                      start: { line: 1, column: 1, offset: 0 },
+                      end: { line: 1, column: 11, offset: 10 }
+                  }
+              }
+          ],
+          position: {
+              start: { line: 1, column: 1, offset: 0 },
+              end: { line: 1, column: 11, offset: 10 }
+          }
+      }
+  ;
+
+  const parsedMarkdown = parseMarkdown(markDown);
+
+  assertObjectMatch(parsedMarkdown, json);
+});
